24afa1083406c5a2c97aba6a1a4fd8d292ba996a
William Högman Rudenmalm <me@whn.se>
2023-09-29 14:29:19 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: "/",
+        destination: "https://sobel.io/staff/whn",
+        permanent: false,
+      },
+      {
         source: '/readme',
         destination: 'https://github.com/williamhogman/working-with-will/blob/master/README.org',
         permanent: false,
